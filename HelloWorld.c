bf1c62be565b4d584e694d727d6a6f27c0034f4c
S-Nishikawa-VIA <84700613+S-Nishikawa-VIA@users.noreply.github.com>
2022-11-25 11:06:26 +0900

    あいさつ文に「SJIS」と挿入

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-// 標準出力に文章を出力するプログラム（SJIS製ソースコード）
+// 標準出力にSJIS 世界への挨拶文を出力するプログラム（SJIS製ソースコード）
 int main()
 {
-    // 標準出力に Hello World と出力する関数
-    printf("Hello World!");
+    // 標準出力に SJIS 世界への挨拶を出力する関数
+    printf("Hello SJIS World!");
     return 0; // 終了する
 }
