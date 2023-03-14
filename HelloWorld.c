1f0f9d21536e8eaa7735710ff244db6f46e19ccf
park-u <park-u@macnica.co.jp>
2023-03-14 13:37:54 +0900

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,7 +2,7 @@
 // 標準出力に文章を出力するプログラム（SJIS製ソースコード）
 int main()
 {
-    // 標準出力に Hello World と出力する関数asdasd
+    // 標準出力に Hello World と出力する関数asdasddsa
     printf("Hello World!");
     return 0; // 終了する
 }
